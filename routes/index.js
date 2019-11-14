1f6d7a5473be5515372e2a1f7a365016acc4350e
Eudes Lemos <eudesllemos@gmail.com>
2019-11-14 13:24:53 -0300

    Change message return

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-  res.send('Today we will lunch at Rockfella');
+  res.send({
+    "response_type": "in_channel",
+    "type": "mrkdwn",
+    "text": "Today we will lunch at *Rockafella*"
+  });
 });
 
 module.exports = router;
